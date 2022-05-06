56477225a2c391a9481fe4e1c349a76f288186df
qiuzhenyu <catnuko6912@gamil.com>
2022-05-06 10:22:31 +0800

    新增材质label

--- src/views/cesium/map.ts
+++ src/views/cesium/map.ts
@@ -3,13 +3,14 @@ export function main(elementId: string, tiandituTk: string) {
 	const viewer = new Cesium.Viewer(elementId, {
 		shadows: true,
 	});
-	console.log(viewer)
-	viewer.scene.globe.depthTestAgainstTerrain = true;
-	const tileset = new Cesium.Cesium3DTileset({
-		url: "/SampleData/Cesium3DTiles/Tilesets/Tileset/tileset.json",
-	});
-	tileset.readyPromise.then(function (tileset) {
-		viewer.scene.primitives.add(tileset);
-		viewer.zoomTo(tileset, new Cesium.HeadingPitchRange(0.0, -0.5, tileset.boundingSphere.radius * 2.0));
-	});
+
+	// console.log(viewer)
+	// viewer.scene.globe.depthTestAgainstTerrain = true;
+	// const tileset = new Cesium.Cesium3DTileset({
+	// 	url: "/SampleData/Cesium3DTiles/Tilesets/Tileset/tileset.json",
+	// });
+	// tileset.readyPromise.then(function (tileset) {
+	// 	viewer.scene.primitives.add(tileset);
+	// 	viewer.zoomTo(tileset, new Cesium.HeadingPitchRange(0.0, -0.5, tileset.boundingSphere.radius * 2.0));
+	// });
 }
